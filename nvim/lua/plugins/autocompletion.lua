4886b2d319831218a565489b6451484797db2d20
Leonardo <leonardokaftal1@gmail.com>
2024-11-21 01:30:48 +0100

    lsp support for various language

--- nvim/lua/plugins/autocompletion.lua
+++ nvim/lua/plugins/autocompletion.lua
@@ -81,9 +81,9 @@ return { -- Autocompletion
 			-- No, but seriously. Please read `:help ins-completion`, it is really good!
 			mapping = cmp.mapping.preset.insert({
 				-- Select the [n]ext item
-				["<A-l>"] = cmp.mapping.select_next_item(),
+				["<A-k>"] = cmp.mapping.select_next_item(),
 				-- Select the [p]revious item
-				["<A-k>"] = cmp.mapping.select_prev_item(),
+				["<A-l>"] = cmp.mapping.select_prev_item(),
 
 				-- Scroll the documentation window [b]ack / [f]orward
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -111,7 +111,7 @@ return { -- Autocompletion
 				--    $body
 				--  end
 				--
-				-- <c-l> will move you to the right of each of the expansion locations.
+				-- <C-l> will move you to the right of each of the expansion locations.
 				-- <c-h> is similar, except moving you backwards.
 				["<C-l>"] = cmp.mapping(function()
 					if luasnip.expand_or_locally_jumpable() then
